49012a74add88051b962b759c4d722b695b63052
shyuwang <sw3449@columbia.edu>
2025-11-14 17:52:36 -0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -6,7 +6,7 @@ import os
 
 def train_model():
     # Load data
-    data = fetch_california_housing()
+    data = fetch_california_housing(as_frame=True, download_if_missing=True)
     X_train, X_test, y_train, y_test = train_test_split(
         data.data, data.target, test_size=0.2, random_state=42
     )
